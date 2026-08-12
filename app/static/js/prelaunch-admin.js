fc3775056820c9006543035163d8f14f779b895a
AIestimGRO <mrgrom2014@gmail.com>
2026-08-12 11:48:18 +0300

    Route new JACKSIDE creation through daily issue screen

--- app/static/js/prelaunch-admin.js
+++ app/static/js/prelaunch-admin.js
@@ -1,6 +1,20 @@
 (() => {
   const centralizeCampaignEconomy = () => {
     if (location.pathname !== '/master') return;
+
+    const createForm = document.querySelector('form.campaign-create');
+    const typeSelect = createForm?.querySelector('select[name="campaign_type"]');
+    const dailyOption = typeSelect?.querySelector('option[value="daily_414"]');
+    if (dailyOption) {
+      dailyOption.remove();
+      if (!createForm.querySelector('.jackside-create-route-note')) {
+        const note = document.createElement('p');
+        note.className = 'muted jackside-create-route-note';
+        note.innerHTML = 'Новые ежедневные JACKSIDE создаются через <a href="/master/jackside-issues">«Выпуски JACKSIDE»</a>: дата, 18:14 и экономика JC подставляются централизованно.';
+        createForm.prepend(note);
+      }
+    }
+
     document.querySelectorAll('.campaign-jackcoin-fields').forEach((fieldset) => {
       ['jackcoin_per_correct', 'jackcoin_completion_bonus', 'jackcoin_perfect_bonus'].forEach((name) => {
         const input = fieldset.querySelector(`[name="${name}"]`);
